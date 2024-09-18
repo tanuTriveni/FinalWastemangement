a10488ac7b4c761f2d8130ce60ff7459a0910610
Triveni Kotgule <kotguletriveni@gmail.com>
2024-09-18 20:04:28 +0530

    Your commit message

--- config/db.js
+++ config/db.js
@@ -1,13 +1,19 @@
-//configuration related to mongodb
 const mongoose = require('mongoose');
-const colors = require('colors');
-const connectDB = async() =>{
-    try{
-await mongoose.connect("mongodb+srv://kotguletriveni:MEoOAt7I6z736vC1@wastemanage.xcppozp.mongodb.net/wastemanage?retryWrites=true&w=majority")
-console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white);
-    }
-    catch(error){
-console.log(`Mongodb Server Issue ${error} `.bgRed.white)
-    }
-}
-module.exports= connectDB;
+const colors = require('colors');  // Ensure you have colors installed using 'npm install colors'
+const dotenv = require('dotenv');
+
+// Load environment variables from `.env` file
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    // Use the environment variable for MongoDB URL
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log(`Mongodb connected: ${mongoose.connection.host}`.bgGreen.white);
+  } catch (error) {
+    console.log(`Mongodb Server Issue: ${error.message}`.bgRed.white);
+    process.exit(1);  // Exit process with failure
+  }
+};
+
+module.exports = connectDB;
